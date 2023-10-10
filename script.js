0f47c56701c8a7fd87f0a5f6410086918ea5f696
aheath <aaron.heath5447@gmail.com>
2023-10-10 18:37:57 -0400

    Add pseudocode and wire-framing

--- script.js
+++ script.js
@@ -1,9 +1,63 @@
+var generatePassword = function(characterPool, pwdLength) {
+  // Your generate password function goes here.
+  var password= "";
+  for(let i = 0; i < pwdLength; i ++) {
+    
+  }
+}
+
+// Returns Number if all digits of the String are numeric. Else returns false.
+var validateNumber = function(string) {
+  for (let i = 0; i < string.length; i ++) {
+    const NUMBERS = ['0','1','2','3','4','5','6','7','8','9'];
+    if(NUMBERS.includes(string[i])) {
+      if (i + 1 == string.length) {
+        return parseInt(string);
+      }
+      continue;
+    } else {
+      return false;
+    }
+  }
+}
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var input = prompt("Give me input")
+  var pwdLength = validateNumber(prompt("Enter the password length:\nMin: 8, Max: 128", 8));
+  
+  if(!pwdLength) {
+    console.log(pwdLength)
+    return; 
+  }
+  
+  if (pwdLength === null) {
+    return;
+  }
+
+
+  // Returns if  user does not enter a NUMBER between 8 and 128
+  if (pwdLength < 8 || pwdLength > 128 || Number.isNaN(pwdLength)) {
+    alert("Try again");
+    console.log(pwdLength);
+    return null;
+  }
+
+  var includeLower = window.confirm("Click okay to use lowercase characters");
+  var includeUpper = window.confirm("Upper");
+
+  // var characterPool = [];
+
+  // if(includeLower) {
+  //   const lower = []
+  //   characterPool=characterPool.concat(lower);
+  // }
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -13,3 +67,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// Prompt user for password length
+// Verify Password length
+// Confirm lower case
+// Confirm upper case
+// Confirm numeric characters
+// Confirm special characters
+
+// Implement generatePassword() using those parameters
+  // if lowerCase
+  // if upperCase
+  // if numeric
+  // specialCharacters
+  // return password
